110c89cc2bacfb1547e6921ab8bda37f2e961ea3
senaboyuktas <senabykts99@gmail.com>
2023-07-29 20:30:50 +0300

    46. ders sonu

---
+++ Core_Proje/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar() 
+        { 
+            return PartialView();
+        }
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Core_Proje/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+    }
+}
